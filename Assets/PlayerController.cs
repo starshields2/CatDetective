0e3542f43a947f48aabb31e390cf7c3fbe8d70f2
starshields2 <139510542+starshields2@users.noreply.github.com>
2026-04-26 17:31:05 -0500

    Movement Demo

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,30 +5,50 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 10f;
     public Vector3 movetarget;
+    public bool isClimbing;
+    private Rigidbody2D rb;
 
     void Start()
     {
         movetarget = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             movetarget = new Vector3(
                 mousePos.x,
-                transform.position.y,
+                -4.24f,
                 transform.position.z
             );
         }
 
-        // This stays exactly the same
+        
         transform.position = Vector3.MoveTowards(
             transform.position,
             movetarget,
             speed * Time.deltaTime
         );
+
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        else
+        {
+            rb.gravityScale = 1f;
+        }
+    }
+
+
+    void HandleLedges()
+    {
+      
     }
 }
--- Assets/TriggerPlayerJump.cs
+++ Assets/TriggerPlayerJump.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class TriggerPlayerJump : MonoBehaviour
 {
+    public PlayerController pControl;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pControl = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            pControl.isClimbing = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            pControl.isClimbing = false;
+        }
     }
 }
