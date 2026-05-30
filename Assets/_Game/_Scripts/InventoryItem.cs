92625e34de4e40ac9c293d36f8465a7b7a43e2e4
starshields2 <139510542+starshields2@users.noreply.github.com>
2026-05-30 16:55:38 -0500

    Inventory Setup

--- Assets/_Game/_Scripts/InventoryItem.cs
+++ Assets/_Game/_Scripts/InventoryItem.cs
@@ -7,12 +7,16 @@ public class InventoryItem : MonoBehaviour
 {
     public Item _itemData;
     public Image _DisplayIcon;
-     
+    [SerializeField] private Button _inventoryButton;
+    public bool isKeyItem;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
+    
 
     // Update is called once per frame
     void Update()
@@ -24,5 +28,20 @@ public class InventoryItem : MonoBehaviour
     {
         this.gameObject.name = _itemData._itemName;
         _DisplayIcon.sprite = _itemData.icon;
+        _inventoryButton = GetComponent<Button>();
+
+        _inventoryButton.onClick.AddListener(UseItem);
+    }
+
+    public void UseItem()
+    {
+        Debug.Log("Used Item. Meow.");
+        if (isKeyItem)
+        {
+            Debug.Log("Meow, correct item.");
+            //feedback
+            //do item thing.
+            //destroy item
+        }
     }
 }
